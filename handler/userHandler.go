6c08702241ecf1f9ea9a22ac1f00fc3816d32986
yyhmmwan@gmail.com <yyhmmwan@gmail.com>
2020-08-26 22:02:50 +0800

    add user handler

--- handler/userHandler.go
+++ handler/userHandler.go
@@ -1,32 +1,36 @@
 package handler
 
+import (
+	"github.com/labstack/echo/v4"
+	"go_echo_ent/ent"
+)
+
 /**
 UserHandler
 */
 
 // 创建新用户
-//func createUser() echo.HandlerFunc {
-//	return func(c echo.Context) error {
-//		//var user  ent.User
-//		m := echo.Map{}
-//		if err := c.Bind(&m); err != nil {
-//			return err
-//		}
-//		u := ent.User{
-//			Name: m["name"].(string),
-//			Age: m["age"].(int),
-//			Username: m["username"].(string),
-//			Email: m["email"].(string),
-//			Password : m["password"].(string),
-//
-//		}
-//
-//		//user = u
-//		//_, err := ent.Client.User.Create()
-//
-//		return nil
-//	}
-//}
+func createUser() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var _ ent.User
+		m := echo.Map{}
+		if err := c.Bind(&m); err != nil {
+			return err
+		}
+		u := ent.User{
+			Name:     m["name"].(string),
+			Age:      m["age"].(int),
+			Username: m["username"].(string),
+			Email:    m["email"].(string),
+			Password: m["password"].(string),
+		}
+
+		_ = u
+		//_, err := ent.Client.User.Create()
+
+		return nil
+	}
+}
 
 // 根据用户ID查询用户
 func getUserById() {
